76201ba453c27afcaf177a4ca09fbd7bf79d8145
Emad4770 <emad47.n7@gmail.com>
2024-11-14 00:31:27 +0100

    Update .gitignore to include jsconfig.json; refactor dao and server files for consistent string formatting

--- dao.mjs
+++ dao.mjs
@@ -1,30 +1,30 @@
-import pg from "pg";
-import fs from "fs";
+import pg from 'pg';
+import fs from 'fs';
 
 // Database connection details
 const client = new pg.Client({
-  host: "127.0.0.1",
+  host: '127.0.0.1',
   port: 5432,
-  user: "postgres",
-  password: "admin",
-  database: "postgres",
+  user: 'postgres',
+  password: 'admin',
+  database: 'postgres',
 });
 
 async function connect() {
   try {
     await client.connect();
-    console.log("Connected to the database");
+    console.log('Connected to the database');
   } catch (err) {
-    console.error("Error connecting to the database", err.stack);
+    console.error('Error connecting to the database', err.stack);
   }
 }
 
 async function disconnect() {
   try {
     await client.end();
-    console.log("Disconnected from the database");
+    console.log('Disconnected from the database');
   } catch (err) {
-    console.error("Error disconnecting from the database", err.stack);
+    console.error('Error disconnecting from the database', err.stack);
   }
 }
 
@@ -49,14 +49,14 @@ async function getGeoData() {
     const geoJSON = res.rows[0].geojson;
 
     fs.writeFileSync(
-      "./output/output.geojson",
+      './output/output.geojson',
       JSON.stringify(geoJSON, null, 2)
     );
-    console.log("GeoJSON data written to output.geojson");
+    console.log('GeoJSON data written to output.geojson');
 
     return geoJSON;
   } catch (err) {
-    console.error("Error executing query", err.stack);
+    console.error('Error executing query', err.stack);
   }
 }
 
--- server.mjs
+++ server.mjs
@@ -1,14 +1,14 @@
-import express from "express";
-import dao from "./dao.js";
+import express from 'express';
+import dao from './dao.js';
 
 const app = express();
 
-app.get("/getgeo", (req, res) => {
+app.get('/getgeo', (req, res) => {
   dao.getGeoData().then((data) => {
     res.json(data);
   });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
